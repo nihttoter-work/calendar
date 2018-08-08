1fa6161742e2b1568086e34058e8cc56c1a4f51d
Vasechko <Vasechko@binbank.ru>
2018-08-08 12:48:31 +0300

    Add markup.

--- src/app/modules/calendar/calendar.module.ts
+++ src/app/modules/calendar/calendar.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { CalendarTitleComponent } from './components/calendar-title/calendar-title.component';
 import { DayComponent } from './components/day/day.component';
+import { CalendarHeaderComponent } from './components/calendar-header/calendar-header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CalendarComponent, CalendarTitleComponent, DayComponent]
+  declarations: [CalendarComponent, CalendarTitleComponent, DayComponent, CalendarHeaderComponent]
 })
 export class CalendarModule { }
