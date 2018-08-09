76b2fa87d22b377b3c414f8c1997e7ffdadbb9ae
Vasechko <Vasechko@binbank.ru>
2018-08-09 18:25:40 +0300

    Format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { CalendarModule } from './modules/calendar/calendar.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CalendarModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CalendarModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/calendar/calendar.module.ts
+++ src/app/modules/calendar/calendar.module.ts
@@ -4,11 +4,15 @@ import { CalendarComponent } from './components/calendar/calendar.component';
 import { CalendarTitleComponent } from './components/calendar-title/calendar-title.component';
 import { DayComponent } from './components/day/day.component';
 import { CalendarHeaderComponent } from './components/calendar-header/calendar-header.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [CalendarComponent, CalendarTitleComponent, DayComponent, CalendarHeaderComponent]
+  imports: [CommonModule, SharedModule],
+  declarations: [
+    CalendarComponent,
+    CalendarTitleComponent,
+    DayComponent,
+    CalendarHeaderComponent
+  ]
 })
-export class CalendarModule { }
+export class CalendarModule {}
